bd7586cb83adf1a094a015a51ec8fdf5bfc93f50
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-29 19:01:05 +0700

    Auth route added for future.

--- task-manager-client/src/router/index.ts
+++ task-manager-client/src/router/index.ts
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AuthView from '../views/AuthView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/auth',
+      name: 'auth',
+      component: AuthView,
     },
     {
-      path: '/task-manager',
+      path: '/',
       name: 'task-manager',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
