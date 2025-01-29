28f6079d340b72bdce5bb772388cba368575334d
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-29 19:02:08 +0700

    WebConfig upd CORS now not blocking requests from frontend.

--- task-manager-server/src/main/java/com/github/nibavs/task_manager_server/WebConfig.java
+++ task-manager-server/src/main/java/com/github/nibavs/task_manager_server/WebConfig.java
@@ -8,6 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
