7ce88837e77b0ccdc270fd0ed44316c29154c994
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-29 18:59:03 +0700

    Refactor

--- task-manager-client/src/App.vue
+++ task-manager-client/src/App.vue
@@ -1,36 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/task-manager">Task Manager</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
-<style>
-body {
-  display: flex;
-  align-items: start;
-}
-#app {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  /* justify-content: center; */
-}
-</style>
 
 <style scoped>
 header {
