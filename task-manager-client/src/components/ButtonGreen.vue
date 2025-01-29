4a9bebf09f7a47f196b2825870b3050719e83fba
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-29 18:59:46 +0700

    New button component created.

---
+++ task-manager-client/src/components/ButtonGreen.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <button class="button-green">
+    {{ title }}
+  </button>
+</template>
+
+
+<style scoped>
+.button-green {
+  background-color: #006838;
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  border-radius: 5px;
+  padding: 8px 15px;
+  border: 1px solid #006838;
+}
+.button-green:hover {
+  background-color: #045630;
+
+}
+.button-green:active {
+  background-color: #2e835c;
+}
+
+/* @media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+} */
+
+/* <template #icon>
+      <DocumentationIcon />
+    </template> */
+</style>
