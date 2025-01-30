8a3848a94c3af37abf189e5c98f6ee57ce9335e1
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-30 20:35:08 +0700

    TaskItemPrettier.

--- task-manager-client/src/components/TaskItem.vue
+++ task-manager-client/src/components/TaskItem.vue
@@ -1,36 +1,68 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import ButtonGreen from '@/components/ButtonGreen.vue'
+import type { Task } from '@/types'
 
-interface Task {
-  id: number
-  title: string
-  description: string
-  status: string
-  createdAt: string
+defineProps<{ task: Task }>()
+
+// const formatDate = (dateString: string) => {
+//   const dateOfCreation = new Date(dateString).getTime();
+//   const dateNow = new Date().getTime();
+//   const diffTime = Math.abs(dateNow - dateOfCreation);
+//   const diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+//   return diffHours + ' h'
+// }
+
+const formatStatus = (statusString: string) => {
+  return (
+    statusString.charAt(0) +
+    statusString.slice(1, statusString.length).toLowerCase().replace('_', ' ')
+  )
+}
+const statusBgColor = (statusString: string) => {
+  switch (statusString) {
+    case 'NEW':
+      return '#fff'
+    case 'IN_PROGRESS':
+      return '#ff8a8a'
+    case 'COMPLETED':
+      return '#006838'
+  }
 }
 
-const props = defineProps<{ task: Task }>()
-
-const formatDate = (dateString: string) => {
-  const dateOfCreation = new Date(dateString).getTime();
-  const dateNow = new Date().getTime();
-  const diffTime = Math.abs(dateNow - dateOfCreation);
-  const diffHours = Math.floor(diffTime / (1000 * 60 * 60));
-  return diffHours + ' h'
-}
-
-const formatStatus = (statusString : string) => {
-  return statusString.charAt(0) + statusString.slice(1, statusString.length).toLowerCase().replace('_', ' ');
+const statusTextColor = (statusString: string) => {
+  switch (statusString) {
+    case 'NEW':
+      return '#000'
+    case 'IN_PROGRESS':
+      return '#fff'
+    case 'COMPLETED':
+      return '#fff'
+  }
 }
 </script>
 
 <template>
   <li class="task-item">
     <p class="task-item__title">{{ task.title }}</p>
-    <p class="task-item__status">{{ formatStatus(task.status) }}</p>
-    <!-- {{ formatDate(task.createdAt) }} -->
-    <ButtonGreen title="X" @click="$emit('delete', task.id)" />
+    <p
+      class="task-item__status"
+      :style="{
+        backgroundColor: statusBgColor(task.status),
+        color: statusTextColor(task.status),
+        borderColor: statusTextColor(task.status),
+      }"
+    >
+      {{ formatStatus(task.status) }}
+    </p>
+    <ButtonGreen
+      title="x"
+      bgColor="#fff"
+      textColor="#006838"
+      borderColor="#006838"
+      border-radius="50%"
+      @click="$emit('delete', task.id)"
+    />
   </li>
 </template>
 
@@ -40,7 +72,7 @@ const formatStatus = (statusString : string) => {
   justify-content: space-between;
   align-items: center;
   padding: 10px 20px;
-  border: 2px solid #006838;
+  border: 2px solid #d9d9d9;
   border-radius: 15px;
   font-size: 20px;
   font-weight: 600;
@@ -50,16 +82,13 @@ const formatStatus = (statusString : string) => {
 
 .task-item__title {
   text-align: left;
-  min-width: 300px;
-
+  width: 300px;
 }
 
 .task-item__status {
   text-align: center;
   min-width: 150px;
-  border: 1px solid #006838;
-  background-color: #006838;
-  color: #FFF;
   border-radius: 30px;
+  border: 1px solid;
 }
 </style>
