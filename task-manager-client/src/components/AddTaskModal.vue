931de66c8a8fd6095fcb1c5fda80ce9db2385b9b
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-30 20:34:19 +0700

    Add task Modal done.

---
+++ task-manager-client/src/components/AddTaskModal.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ButtonGreen from './ButtonGreen.vue';
+
+const emit = defineEmits(['close', 'addTask']);
+const title = ref('');
+const description = ref('');
+const status = ref('NEW');
+
+const submitTask = () => {
+  emit('addTask', {
+    title: title.value,
+    description: description.value,
+    status: status.value
+  })
+  emit('close');
+}
+</script>
+
+<template>
+  <div class="modal__overlay">
+    <div class="modal">
+      <div class="modal__header">
+        <h1>Add Task</h1>
+      </div>
+      <form action="" class="modal__form">
+        <label for="title">Title</label>
+        <input type="text" name="Title" id="title" v-model="title" required />
+        <label for="desc">Description</label>
+        <input type="text" name="Description" id="desc" v-model="description" required />
+
+        <label for="status">Status</label>
+        <select name="Status" id="status" v-model="status" required>
+          <option value="NEW">NEW</option>
+          <option value="IN_PROGRESS">IN_PROGRESS</option>
+          <option value="COMPLETED">COMPLETED</option>
+        </select>
+      </form>
+
+      <div class="modal__buttons">
+        <ButtonGreen title='Cancel' bgColor='#fff' textColor='#000' borderColor='#000' @click="emit('close')"/>
+        <ButtonGreen title='Add task' @click="submitTask"/>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.modal__overlay {
+  padding: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0,0,0,0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  width: 400px;
+  min-height: 550px;
+  margin: 0 50px;
+  padding: 30px 70px;
+  position: relative;
+  background-color: #FFF;
+  border-radius: 20px;
+}
+
+.modal__form {
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+  font-weight: 600;
+}
+
+.modal__form label {
+  margin: 20px 0 5px;
+}
+
+.modal__form input,
+.modal__form select {
+  height: 40px;
+  font-size: 16px;
+  padding: 0 10px;
+}
+
+
+.modal__buttons {
+  margin-top: 80px;
+  display: flex;
+  justify-content: space-between;
+}
+
+
+</style>
