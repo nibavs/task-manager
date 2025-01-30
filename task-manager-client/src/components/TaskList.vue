ef1396f5e8c1efe136a0cab64914e57bfa9add74
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-30 20:35:28 +0700

    TaskList added.

---
+++ task-manager-client/src/components/TaskList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import TaskItem from '@/components/TaskItem.vue'
+import type { Task } from '@/types'
+
+defineProps<{ tasks: Task[] }>()
+const emit = defineEmits(['delete-task'])
+</script>
+
+<template>
+  <ul class="task-list">
+    <TaskItem
+      v-for="task in tasks"
+      :key="task.id"
+      :task="task"
+      @delete="emit('delete-task', task.id)"
+    />
+  </ul>
+</template>
+
+<style scoped>
+.task-list {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
