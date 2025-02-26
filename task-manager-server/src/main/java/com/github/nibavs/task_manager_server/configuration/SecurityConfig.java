1fb7c822e8cf5568b6a1f807256e3e438ac44701
Nikita Bavs <nbavsunovsky@gmail.com>
2025-02-26 22:26:17 +0700

    JwtFilterBefore added.

--- task-manager-server/src/main/java/com/github/nibavs/task_manager_server/configuration/SecurityConfig.java
+++ task-manager-server/src/main/java/com/github/nibavs/task_manager_server/configuration/SecurityConfig.java
@@ -1,24 +1,32 @@
-package com.github.nibavs.task_manager_server;
+package com.github.nibavs.task_manager_server.configuration;
 
+import com.github.nibavs.task_manager_server.jwt.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/tasks/**").authenticated()
+                        .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+
     }
 }
