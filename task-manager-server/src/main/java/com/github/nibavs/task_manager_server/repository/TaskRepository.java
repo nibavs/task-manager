336b3ebcfbb948610d8b887026e281c0deb2d8e7
Nikita Bavs <nbavsunovsky@gmail.com>
2025-02-26 22:28:29 +0700

    findAllByPerson_Login method added.

--- task-manager-server/src/main/java/com/github/nibavs/task_manager_server/repository/TaskRepository.java
+++ task-manager-server/src/main/java/com/github/nibavs/task_manager_server/repository/TaskRepository.java
@@ -1,9 +1,12 @@
-package com.github.nibavs.task_manager_server;
+package com.github.nibavs.task_manager_server.repository;
 
+import com.github.nibavs.task_manager_server.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+    List<Task> findAllByPerson_Login(String login);
 }
