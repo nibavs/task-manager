13eb3aea886bde0d06abcfe393570dac1f37f60d
Nikita Bavs <nbavsunovsky@gmail.com>
2025-03-01 18:46:39 +0700

    SignUp added. Controller done.

--- task-manager-server/src/main/java/com/github/nibavs/task_manager_server/controller/AuthController.java
+++ task-manager-server/src/main/java/com/github/nibavs/task_manager_server/controller/AuthController.java
@@ -1,38 +1,51 @@
 package com.github.nibavs.task_manager_server.controller;
 
-import com.github.nibavs.task_manager_server.exception.UserNotFoundException;
+import com.github.nibavs.task_manager_server.entity.User;
+import com.github.nibavs.task_manager_server.exception.UserAlreadyExistsException;
+import com.github.nibavs.task_manager_server.exception.InvalidUsernameOrPasswordException;
 import com.github.nibavs.task_manager_server.jwt.JwtUtil;
-import com.github.nibavs.task_manager_server.entity.Person;
-import com.github.nibavs.task_manager_server.repository.PersonRepository;
+import com.github.nibavs.task_manager_server.repository.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/auth")
+@Tag(name = "Auth", description = "Sign in / sign up")
 public class AuthController {
-    @Autowired
-    private PersonRepository personRepository;
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    @Operation(summary = "Sign in, get JWT token")
-    @PostMapping("api/auth/signin")
-    public ResponseEntity<?> signIn(@RequestBody Person person) {
-        Person foundedPerson = personRepository.findByLogin(person.getLogin())
-                .orElseThrow(() -> new UserNotFoundException(person.getLogin()));
-
-        if (foundedPerson.getPasswordHash().equals(person.getPasswordHash())) {
-            String token = jwtUtil.generateToken(foundedPerson.getLogin());
-            return ResponseEntity.ok(token);
-        }
-
-        // use global error handling throw...
-
-        return ResponseEntity.notFound().build();
+    public AuthController(UserRepository userRepository, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
     }
 
+    @Operation(summary = "Sign in, get JWT token")
+    @PostMapping("/signIn")
+    public ResponseEntity<String> signIn(@RequestBody User user) {
+        User foundedUser = userRepository.findByUsername(user.getUsername())
+                .orElseThrow(InvalidUsernameOrPasswordException::new);
+
+        if (!foundedUser.getPassword().equals(user.getPassword())) {
+            throw new InvalidUsernameOrPasswordException();
+        }
+
+        String token = jwtUtil.generateToken(foundedUser.getUsername());
+        return ResponseEntity.ok(token);
+    }
+
+    @Operation(summary = "Sign up, get JWT token")
+    @PostMapping("/signUp")
+    public ResponseEntity<String> signUp(@RequestBody User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new UserAlreadyExistsException(user.getUsername());
+        }
+
+        userRepository.save(user);
+
+        String token = jwtUtil.generateToken(user.getUsername());
+        return ResponseEntity.ok(token);
+    }
 }
